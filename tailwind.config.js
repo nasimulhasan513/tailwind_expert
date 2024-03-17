298959026f75d9a502d97fd2663d952a37520d73
nasimulhasan <nasimulhasan@gmail.com>
2024-03-17 19:04:28 +0600

    Tailwind Expert Task Submission

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#5358b4'
+      }
+    },
+  },
+  plugins: [],
+}
+
